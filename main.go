ca6420fc40b264367785d214893f071f21ff42f9
ililiiilil <gjwnsgod1@naver.com>
2021-09-26 18:41:22 +0900

    practice

--- main.go
+++ main.go
@@ -2,19 +2,44 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/ililiiilil/learngo/accounts"
+	"net/http"
 )
 
-func main() {
-	account := accounts.NewAccount("nico")
-	account.Deposit(10)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(20)
+type result struct {
+	url    string
+	status string
+}
 
-	if err != nil {
-		fmt.Println(err)
+func main() {
+	c := make(chan result)
+
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
 
-	fmt.Println(account.Balance(), account.Owner())
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		fmt.Println(<-c)
+	}
+}
+
+func hitURL(url string, c chan<- result) {
+	resp, err := http.Get(url)
+	status := "OKAY"
+
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	c <- result{url: url, status: status}
 }
